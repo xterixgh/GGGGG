083cbcb509f546a3eed3217d01cec004a634021a
212323 <212323@1-1220-08>
2025-04-08 16:46:28 +0300

    Добавьте файлы проекта.

---
+++ GGGGG/Pages/AnketaPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GGGGG.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AnketaPage.xaml
+    /// </summary>
+    public partial class AnketaPage : Page
+    {
+        public AnketaPage()
+        {
+            InitializeComponent();
+        }
+    }
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Autor { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ GGGGG/Pages/AutorizationPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GGGGG.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AutorizationPage.xaml
+    /// </summary>
+    public partial class AutorizationPage : Page
+    {
+        public AutorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var login = loginTb.Text;
+            var password = PasswordTb.Password;
+            var loginUser = "Admin";
+            var passwordUser = "Admin";
+            if (login == loginUser && password == passwordUser)
+            {
+                MessageBox.Show("Здравствуй админ");
+                NavigationService.Navigate(new AnketaPage());
+            }
+            else
+            {
+                MessageBox.Show("Уолтер убери свой ****");
+            }
+        }
+    }
+}
